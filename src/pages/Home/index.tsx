812b84838ab79a6387a3abfe267b32df999f8202
Jonathan dos Santos <jthan.lopes@gmail.com>
2023-01-02 15:12:06 -0300

    feat: estrutura da página Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,32 @@
+import { Play } from 'phosphor-react'
+
+export function Home() {
+  return (
+    <div>
+      <form action="">
+        <div>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input type="text" id="task" />
+
+          <label htmlFor="minuteAmount">durante</label>
+          <input type="number" id="minutesAmount" />
+
+          <span>minutos.</span>
+        </div>
+
+        <div>
+          <span>0</span>
+          <span>0</span>
+          <span>:</span>
+          <span>0</span>
+          <span>0</span>
+        </div>
+
+        <button type="submit">
+          <Play size={24} />
+          Começar
+        </button>
+      </form>
+    </div>
+  )
+}
